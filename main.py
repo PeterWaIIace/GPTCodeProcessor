b6bbf261363b07915c6074c19b440a89286997fc
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-01-08 23:33:07 +0000

    changing prompt

--- main.py
+++ main.py
@@ -38,15 +38,15 @@ class CodeGenerator():
     def __init__(self,language,program_input_description,program_output_description,
                 program_description=""):
 
-        self.init_message = f"I want you to write program in {language}. "+\
+        self.init_message = f"I want you to write program in {language}. You are allowed to respond only in JSON. No explanation. No English text."+\
                             f"\nProgram: {program_description}"+\
                             f"\nProgram Input: {program_input_description}"+\
                             f"\nProgram Output: {program_output_description}"+\
-                            f"\nRespond with JSON having two fields \"CODE\" with {language} code and \"MESSAGE\" with explanation or comment. No text beyond JSON."
+                            f"\nRespond with JSON having one field \"CODE\" with {language} code."
 
+        print(self.init_message)
         self.chatbot = ChatTuned(self.init_message)
         # https://github.com/acheong08/ChatGPT/wiki/Setup
-        self.message  = ""
         self.filename = "dummy.py"
         pass
 
@@ -58,50 +58,56 @@ class CodeGenerator():
         return self.chatbot.ask("{\"Output\":\""+ str(e) +"\"}")
 
     def request_code(self,message):
-        message = ""
+        code = ""
         self.response = self.chatbot.ask(message)
 
         correct_format = False
         while not correct_format:
             try:
+                print(self.response)
                 first_pos   = self.response.find('{')
                 second_pos  = len(self.response) - self.response[-1:].find('}')
                 tmp_message = self.response[first_pos:second_pos]
 
-                message  = json.loads(tmp_message)
+                code  = json.loads(tmp_message)
                 correct_format = True
 
             except Exception as e:
+                print(f"exception: {e}")
                 self.response = self.send_exception(e)
 
-        # if "FILENAME" in self.message.keys():
-        #     self.filename = self.message["FILENAME"]
+        print(f"code: {code}")
+        return code
 
-        return message
-
-    def update_file(self):
-        if self.message["CODE"] != "":
+    def update_file(self,code):
+        if "CODE" in code:
             with open(self.filename,"w") as f:
-                f.write(self.message["CODE"])
+                f.write(code["CODE"])
 
     def run(self):
-        self.request_code(self.init_message)
-        self.update_file()
-        while(self.message["CODE"] != ""):
-            self.run_file()
+        code = self.request_code(self.init_message)
+        self.update_file(code)
+
+        while("CODE" in code):
+
+            output = self.run_file()
+
+            if output:
+                code = self.request_code("{\"Result\":\""+output.decode("utf-8")+"\"}")
+                self.update_file(code)
+
 
     def run_file(self):
         os.environ['PYTHONUNBUFFERED'] = '1'
         process = subprocess.Popen(f"python3 {self.filename}", shell=False, stdout=subprocess.PIPE, env=os.environ) # Shell doesn't quite matter for this issue
         while True:
             output = process.stdout.readline()
+
             if process.poll() is not None:
                 break
-            if output:
-                self.request_code("{\"Result\":\""+output.decode("utf-8")+"\"}.")
-                self.update_file()
+
         rc = process.poll()
-        return rc
+        return output
 
 # returns {'message':message, 'conversation_id':self.conversation_id, 'parent_id':self.parent_id}
 generator = CodeGenerator("Python","No input","Hello World","Program generates and prints output")
