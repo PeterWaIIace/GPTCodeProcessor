5cda10004a24186b51f62b8656b89ebf545a9cf6
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-01-12 18:35:43 +0000

    new interface

--- bottle_frontend/interface.js
+++ bottle_frontend/interface.js
@@ -1,23 +1,37 @@
 const {useState} = React;
 
-const init_prompt = "I want you to write program in Python. You are allowed to respond only in JSON. No explanation. No English text.\n"+
-"Program: Program generates and prints output\n"+
-"Program Input: No input\n"+
-"Program Output: Hello World\n"+
-"Respond with JSON having one field \"CODE\" with Python code.";
+let language_placeholder = "Python"
 
-let prompt = init_prompt;
+let prompt_components = {
+    "prompt_start"            :"I want you to write program in Python. You are allowed to respond only in JSON. No explanation. No English text.\n"+
+    "Program: Program generates and prints output\n",
+    "init_prompt_input_reqs"  :"Program Input: ",
+    "input_reqs"              : "No input\n",
+    "init_prompt_output_reqs" : "Program Output: ",
+    "output_reqs"             : "Hello World\n",
+    "init_prompt_closing"     : "Respond with JSON having one field \"CODE\" with Python code."
+};
+
+function build_prompt()
+{
+    let prompt = "";
+    for (const key in prompt_components)
+    {
+        prompt += prompt_components[key];
+    };
+    return prompt;
+}
 
 function CodeView()
 {
     const [displayedText, setDisplayedText] = useState("");
     // // TO DO: make fetch request to local server
     setInterval(function() {
-        fetch(`${window.location.origin}/read/dummy.py`)
+        fetch(`${window.location.origin}/read/dummy.py`, {cache: "no-store"})
         .then(response => response.text())
         .then(text     => setDisplayedText(text))
         .catch(error   => console.error('Error:', error));
-    }, 1000);
+    }, 1500);
 
     return (
         <div>
@@ -29,15 +43,26 @@ function CodeView()
 
 function PromptInput()
 {
-    const [message, setMessage] = useState(init_prompt);
+    const [inputReqs, setInputReqs]   = useState(prompt_components["input_reqs"]);
+    const [outputReqs, setOutputReqs] = useState(prompt_components["output_reqs"]);
 
-    const handleMessageChange = event => {
+    const handleOutputReqsChange = event => {
         // 👇️ access textarea value
-        setMessage(event.target.value);
-        prompt = event.target.value;
+        setOutputReqs(event.target.value);
+        prompt_components["output_reqs"] = document.getElementById("output_reqs").textContent;
+    };
+
+    const handleInputReqsChange = event => {
+        // 👇️ access textarea value
+        setInputReqs(event.target.value);
+        prompt_components["input_reqs"] = document.getElementById("input_reqs").textContent;
     };
 
     const onPressGenerate = event => {
+
+        let prompt = build_prompt();
+        prompt.replace(language_placeholder, document.getElementById("dLang").textContent);
+
         fetch(`${window.location.origin}/buttons/start`,
             {
                 method: 'POST',
@@ -67,19 +92,41 @@ function PromptInput()
 
     return (
         <div>
-            <textarea  style={{width: "100%",height: "150px"}}
-                type="text" id="fname" name="fname" value={message} onChange={handleMessageChange}>
-            </textarea>
-            <button onClick={onPressGenerate} className="btn btn-primary m-1" title="Send Prompt">Generate</button>
-            <button onClick={onPressStop} className="btn btn-primary m-1" title="Send Prompt">Stop</button>
+            <div className="row">
+                Language
+                <div className="btn-group">
+                    <button type="button" id="dLang" className="btn btn-primary m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Python
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dLabel">
+                        <a className="dropdown-item" onClick={onDropdownChoice} href="#">Python</a>
+                    </div>
+                </div>
+            </div>
 
-            <div className="btn-group">
-                <button type="button" id="dAPI" className="btn btn-primary m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    revChatGPT
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dLabel">
-                    <a className="dropdown-item" onClick={onDropdownChoice} href="#">revChatGPT</a>
-                    <a className="dropdown-item" onClick={onDropdownChoice} href="#">OpenAIAPI</a>
+            <div className="row">
+                Input Code
+                <textarea  style={{width: "100%",height: "100px"}}
+                    type="text" id="input_reqs" name="fname" value={inputReqs} onChange={handleInputReqsChange}>
+                </textarea>
+                Output of Code
+                <textarea  style={{width: "100%",height: "100px"}}
+                    type="text" id="output_reqs" name="fname" value={outputReqs} onChange={handleOutputReqsChange}>
+                </textarea>
+            </div>
+
+            <div className="row">
+                <button onClick={onPressGenerate} className="btn btn-primary m-1" title="Send Prompt">Generate</button>
+                <button onClick={onPressStop} className="btn btn-primary m-1" title="Send Prompt">Stop</button>
+
+                <div className="btn-group">
+                    <button type="button" id="dAPI" className="btn btn-primary m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        revChatGPT
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dLabel">
+                        <a className="dropdown-item" onClick={onDropdownChoice} href="#">revChatGPT</a>
+                        <a className="dropdown-item" onClick={onDropdownChoice} href="#">OpenAIAPI</a>
+                    </div>
                 </div>
             </div>
         </div>
